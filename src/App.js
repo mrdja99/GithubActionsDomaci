df245a89243957196e8681743c6975c5884a885d
mrdja99 <markorsb@gmail.com>
2024-02-22 22:09:15 +0100

    Promenjen link

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Naučite React
         </a>
       </header>
     </div>
