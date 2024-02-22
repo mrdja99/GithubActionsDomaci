cd9726ef6347a38f3999f094fef3d94197a7d88e
mrdja99 <markorsb@gmail.com>
2024-02-22 22:05:55 +0100

    Promenjen naslov home stranice

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dobrodošli na praznu probnu react aplikaciju za testiranje
         </p>
         <a
           className="App-link"
